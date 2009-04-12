b691b98e9901f4fb64935ebe58a2dd15dec21d85
Abhishek Mishra <abhishekmishra3@gmail.com>
2009-04-12 15:31:51 +0530

    added exercise 24

--- exercise1-24.scm
+++ exercise1-24.scm
@@ -1,31 +1,43 @@
 #lang scheme
 ;section on primes
 
-;smallest divisor (improved to exclude even numbers)
-(define (smallest-divisor n)
-  (find-divisor n 2))
-
+;fast-prime
+;using Fermat's little theorem
 (define (square x) (* x x))
 
-(define (next n)
-  (cond ((= n 2) 3)
-        (else (+ n 2))))
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
 
-(define (find-divisor n test-divisor)
-  (cond ((> (square test-divisor) n) n)
-        ((divides? test-divisor n) test-divisor)
-        (else (find-divisor n (next test-divisor)))))
+(define (myrandom x)
+;  (display (round (* x (random))))
+;  (newline)
+  (round (* x (random))))
 
-(define (divides? a b)
-  (= (remainder b a) 0))
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (myrandom (- n 1)))))
 
+(define (fast-prime? n times)
+  (cond ((= times 0) #t)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else #f)))
+
+;prime? using fast-prime
 (define (prime? n)
-  (= n (smallest-divisor n)))
+  (fast-prime? n 100))
 
 ;check order of growth through a timed test
 (define (timed-prime-test n)
   (newline)
   (display n)
+;  (newline)
   (start-prime-test n (current-process-milliseconds)))
 
 (define (start-prime-test n start-time)
